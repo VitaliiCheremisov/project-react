f1ae7a93ec53151db5507ce8797f17eb82045812
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-01-13 21:50:17 +0300

    Диплом. После 2-го ревью.

--- backend/recipes/utils.py
+++ backend/recipes/utils.py
@@ -1,10 +1,15 @@
+from tabulate import tabulate
+
+
 def calculate_shopping_cart(ingredients):
     """Расчет списка покупок."""
     shopping_list = []
     for ingredient in ingredients:
-        name = f"{ingredient['ingredients__name']}:"
+        name = f"{ingredient['ingredients__name']}"
         amount = f"{ingredient['amounts']}"
         unit = f"{ingredient['ingredients__measurement_unit']}"
-        item = f"{name:<20} {amount:<10} {unit}\n"
+        item = [name, amount, unit]
         shopping_list.append(item)
-    return '\n'.join(shopping_list)
+    return tabulate(shopping_list,
+                    headers=["Ингредиент", "Количество", "Единица измерения"],
+                    tablefmt='grid')
