768cab031bc33751e7cee956189676c8f545f167
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-01-13 13:53:50 +0300

    Диплом. После 2-го ревью.

--- backend/recipes/utils.py
+++ backend/recipes/utils.py
@@ -5,6 +5,6 @@ def calculate_shopping_cart(ingredients):
         name = f"{ingredient['ingredients__name']}:"
         amount = f"{ingredient['amounts']}"
         unit = f"{ingredient['ingredients__measurement_unit']}"
-        item = f"{name:<20} {amount:<10} {unit}"
+        item = f"{name:<20} {amount:<10} {unit}\n"
         shopping_list.append(item)
-    return shopping_list
+    return ''.join(shopping_list)
