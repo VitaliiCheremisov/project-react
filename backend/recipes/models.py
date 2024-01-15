000f480361984d893160f222fe93eb8362ae0154
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2024-01-15 14:34:54 +0300

    Диплом. После 3-го ревью.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import (MinValueValidator,
-                                    ValidationError)
+from django.core.validators import MinValueValidator, ValidationError
 from django.db import models
 
 from foodgram import constants
